3e05c5943ffaf65b2e6b2030b114f09f5147243a
小林　良輝 <yoshikikobayashi@yoshikiMacBook-Pro.local>
2020-01-25 14:02:18 +0900

    [finish]Bookers

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	resources :books
+
+	root to: 'books#top'
+
+	delete 'books/:id'=> 'books#destroy', as: 'destroy_book'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
